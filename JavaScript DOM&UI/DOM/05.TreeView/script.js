76436b2db951623ada2e48cbda98e2cee3245aeb
Sh0nZi <sss17@mail.com>
2014-06-09 16:10:53 +0300

    Damn

---
+++ JavaScript DOM&UI/DOM/05.TreeView/script.js
@@ -0,0 +1,20 @@
+﻿window.onload = function () {
+    var elems = document.getElementsByClassName('changeble');
+
+    for (var i = 0; i < elems.length; i++) {
+        elems[i].addEventListener('click', changeVisibility);
+    }
+    
+    function changeVisibility(event) {
+        var target;
+      
+        target = event.target ? event.target : event.srcElement;
+        if (event.target !== this) {
+            if (target.childNodes[1].style.display === 'none') {
+                target.childNodes[1].style.display = 'block'
+            } else {
+                target.childNodes[1].style.display = 'none'
+            }
+        }
+    }
+}
